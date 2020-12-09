e88313676336270763cf095f75da395408635392
thzero <thzero@users.noreply.github.com>
2020-12-09 16:03:51 -0600

    updates

--- index.js
+++ index.js
@@ -6,17 +6,31 @@ class MdnsResourceDiscoveryService extends ResourceDiscoveryService {
 	constructor() {
 		super();
 
-		this._service = null;
+		this._serviceGrpc = null;
+		this._serviceHttp = null;
+	}
+
+	get allowsHeartbeat() {
+		return true;
 	}
 
 	async cleanup() {
 		if (!this._service)
 			return;
 
-		this._service.advertise().then(() => {
-			// stuff you do when the service is published
-			this._logger.info2(`init DNS published`);
-		});
+		if (this._serviceHttp != null) {
+			this._serviceHttp.advertise().then(() => {
+				// stuff you do when the service is published
+				this._logger.info2(`init http DNS published`);
+			});
+		}
+
+		if (this._serviceGrpc != null) {
+			this._serviceGrpc.advertise().then(() => {
+				// stuff you do when the service is published
+				this._logger.info2(`init grpc DNS published`);
+			});
+		}
 	}
 
 	async _initialize(correlationId, opts) {
@@ -27,20 +41,34 @@ class MdnsResourceDiscoveryService extends ResourceDiscoveryService {
 
 		const name = `${packageJson.name}${namespace}.local`;
 
-		// create a service defining a web server running on port 3000
-		this._service = ciao.getResponder().createService({
+		const optsHttp = {
 			name: name,
 			type: opts.secure ? 'https' : 'http',
-			port: opts.port, // optional, can also be set via updatePort() before advertising
-			txt: { // optional
-				key: "value",
-			}
+			port: opts.port
+		};
+		if (opts.txt && Array.isArray(opts.txt))
+			optsHttp.txt = opts.txt;
+
+		this._serviceHttp = ciao.getResponder().createService(optsHttp);
+		this._serviceHttp.advertise().then(() => {
+			this._logger.info2(`init http DNS published`);
 		});
 
-		this._service.advertise().then(() => {
-			// stuff you do when the service is published
-			this._logger.info2(`init DNS published`);
-		});
+		if (opts.grpc) {
+			const optsGrpc = {
+				name: `{grpc}.name`,
+				type: 'grpc',
+				port: opts.grpc.port
+			};
+
+			if (opts.grpc.txt && Array.isArray(opts.grpc.txt))
+				optsGrpc.txt = opts.grpc.txt;
+
+			this._serviceGrpc = ciao.getResponder().createService(optsGrpc);
+			this._serviceGrpc.advertise().then(() => {
+				this._logger.info2(`init grpc DNS published`);
+			});
+		}
 
 		return this._success(correlationId);
 	}
